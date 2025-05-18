eff0dc8641a218a4bc13f4df7dd1a1a43130b744
bluerat1 <delez.chamberlain@gmail.com>
2025-05-18 17:39:33 +0800

    frontend 5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Router, Routes, Route } from "react-router-dom"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Nav from "./components/navigation/Nav"
